64bfe2f5ab83342c929f51f8319cfe5ab49ad6c3
bacn <mbachmann@trdi.io>
2021-11-18 09:10:08 +0100

    le-04-configuration-magic-initial

--- src/main/java/com/example/demoinitial/DemoInitialApplication.java
+++ src/main/java/com/example/demoinitial/DemoInitialApplication.java
@@ -1,19 +1,27 @@
 package com.example.demoinitial;
 
 import com.example.demoinitial.config.MyComponent;
+import com.example.demoinitial.utils.HasLogger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import javax.annotation.PostConstruct;
+import java.util.Arrays;
+
 @Controller
 @SpringBootApplication
-public class DemoInitialApplication {
+public class DemoInitialApplication implements HasLogger {
 
     @Autowired
     MyComponent myComponent;
+
+    @Autowired
+    private Environment env;
 
     public static void main(String[] args) {
         SpringApplication.run(DemoInitialApplication.class, args);
@@ -23,6 +31,12 @@ public class DemoInitialApplication {
     @ResponseBody
     String home() {
         return "Hello World " + myComponent.getPerson().toString();
+    }
+
+    @PostConstruct
+    public void afterInit() {
+        boolean hasDevProfile = Arrays.asList(env.getActiveProfiles()).contains("dev");
+        getLogger().info("Active Profiles: " + Arrays.toString(env.getActiveProfiles()) + "\n\n");
     }
 
 }
