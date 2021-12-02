d56bacf0943794ce86eb71ea8fd68b490d7fd596
bacn <mbachmann@trdi.io>
2021-12-02 10:12:23 +0100

    refactor websocket package

--- src/main/java/com/example/demoinitial/DemoInitialApplication.java
+++ src/main/java/com/example/demoinitial/DemoInitialApplication.java
@@ -7,8 +7,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.annotation.PostConstruct;
 import java.util.Arrays;
