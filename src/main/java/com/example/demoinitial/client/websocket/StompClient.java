a8b00596d636d499c24bfb28fb41a80b75849e16
e542323 <matthias.bachmann2@sbb.ch>
2023-02-12 13:00:55 +0100

    change spring-boot to version 3.0.2

--- src/main/java/com/example/demoinitial/client/websocket/StompClient.java
+++ src/main/java/com/example/demoinitial/client/websocket/StompClient.java
@@ -1,5 +1,7 @@
 package com.example.demoinitial.client.websocket;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 import org.springframework.messaging.converter.MappingJackson2MessageConverter;
@@ -7,22 +9,30 @@ import org.springframework.messaging.simp.stomp.StompSessionHandler;
 import org.springframework.web.socket.client.WebSocketClient;
 import org.springframework.web.socket.client.standard.StandardWebSocketClient;
 import org.springframework.web.socket.messaging.WebSocketStompClient;
+import org.springframework.web.socket.sockjs.client.SockJsClient;
+import org.springframework.web.socket.sockjs.client.Transport;
+import org.springframework.web.socket.sockjs.client.WebSocketTransport;
 
 /**
  * Stand alone WebSocketStompClient.
  *
  */
-public class StompClient {
+public final class StompClient {
+
+    private StompClient() {
+    }
+
 
     public static void main(String[] args) {
-        WebSocketClient client = new StandardWebSocketClient();
+        List<Transport> transports = new ArrayList<>(1);
+        transports.add(new WebSocketTransport( new StandardWebSocketClient()) );
+        WebSocketClient client = new SockJsClient(transports);
         WebSocketStompClient stompClient = new WebSocketStompClient(client);
-
         stompClient.setMessageConverter(new MappingJackson2MessageConverter());
 
         StompSessionHandler sessionHandler = new MyStompSessionHandler();
-        String URL = "ws://localhost:8080/broadcast";
-        stompClient.connect(URL, sessionHandler);
+        String url = "ws://localhost:8080/broadcast";
+        stompClient.connect(url, sessionHandler);
         // Don't close immediately - Type <Enter> to exit
         new Scanner(System.in).nextLine();
     }
