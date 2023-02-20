eac4ba3a671dc9082037aff10142a0eba640308b
e542323 <matthias.bachmann2@sbb.ch>
2023-02-20 15:15:03 +0100

    change spring-boot to version 3.0.2

--- src/main/java/com/example/demoinitial/client/resttemplate/RestClientJwtAuth.java
+++ src/main/java/com/example/demoinitial/client/resttemplate/RestClientJwtAuth.java
@@ -3,20 +3,25 @@ package com.example.demoinitial.client.resttemplate;
 import com.example.demoinitial.domain.Person;
 import com.example.demoinitial.utils.HasLogger;
 import com.fasterxml.jackson.core.JsonProcessingException;
-import org.apache.tomcat.util.codec.binary.Base64;
-import org.springframework.http.*;
+
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.client.RestTemplate;
 
-import java.nio.charset.Charset;
 import java.util.Collections;
-
+import org.springframework.web.util.WebUtils;
 
 /**
  * Backend must be running
  */
 public class RestClientJwtAuth implements HasLogger {
 
-    private final String autenticateUri = "http://localhost:8080/api/authenticate";
+    private final String autenticateUri = "http://localhost:8080/api/auth/signin";
     final String uri = "http://localhost:8080/api/persons/1";
 
     RestTemplate restTemplate = new RestTemplate();
@@ -26,7 +31,7 @@ public class RestClientJwtAuth implements HasLogger {
     }
     public void start() throws JsonProcessingException {
 
-        String token = getLoginToken("admin@example.com", "admin");
+        String token = getLoginToken("admin", "admin");
 
         HttpEntity<String> request = new HttpEntity<String>(createHeaders(token));
         ResponseEntity<Person> response = restTemplate.exchange(uri, HttpMethod.GET, request, Person.class);
@@ -40,11 +45,11 @@ public class RestClientJwtAuth implements HasLogger {
         String json = getLoginParamsJson(username, password);
         HttpEntity<String> request = new HttpEntity<String>(json, createHeaders());
         ResponseEntity<String> response = restTemplate.postForEntity(autenticateUri, request, String.class);
-        return response.getHeaders().get("Authorization").get(0);
+        return getJwtFromCookies(response);
     }
 
     private String getLoginParamsJson(String username, String password) throws JsonProcessingException {
-        return "{\"email\": " + "\"" + username + "\"" + ", \"password\": " + "\"" + password + "\"" + "}";
+        return "{\"username\": " + "\"" + username + "\"" + ", \"password\": " + "\"" + password + "\"" + "}";
     }
 
     private HttpHeaders createHeaders() {
@@ -57,7 +62,13 @@ public class RestClientJwtAuth implements HasLogger {
     private HttpHeaders createHeaders(String token) {
 
         HttpHeaders headers = createHeaders();
-        headers.set( "Authorization", token );
+        headers.set( "Cookie", token );
         return headers;
+    }
+
+    private String getJwtFromCookies(ResponseEntity<String> response) {
+        HttpHeaders headers = response.getHeaders();
+        return headers.getFirst(HttpHeaders.SET_COOKIE);
+
     }
 }
