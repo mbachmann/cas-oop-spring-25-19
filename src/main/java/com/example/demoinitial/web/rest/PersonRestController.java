e5f1312094c698b053b74472818d832a12da8008
mbachmann <m.bachmann@united.ch>
2025-11-21 18:18:43 +0100

    person rest controller mit error handler

--- src/main/java/com/example/demoinitial/web/rest/PersonRestController.java
+++ src/main/java/com/example/demoinitial/web/rest/PersonRestController.java
@@ -2,8 +2,10 @@ package com.example.demoinitial.web.rest;
 
 import com.example.demoinitial.domain.Person;
 import com.example.demoinitial.repository.PersonRepository;
+import com.example.demoinitial.web.exception.PersonNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -20,5 +22,13 @@ public class PersonRestController {
     @GetMapping({"","/"})
     public List<Person> getPersons() {
         return personRepository.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public Person getPersonById(@PathVariable Long id) {
+        return personRepository.findById(id)
+                .orElseThrow(()
+                        -> new PersonNotFoundException("Get not successful: person with id=" + id
+                        + " not found"));
     }
 }
